ad8039c15904d2cc4117fbda857e2b5463c811df
codbrew <cb535114@scc-mmlab-13.local>
2019-02-19 17:50:38 -0500

    Added Whole Project

---
+++ 3401_Week6_Brewer/Assets/Scripts/EagleController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EagleController : MonoBehaviour
+{
+    //refence to birds rigid body; holds variable
+    public Rigidbody2D birdRB;
+    //variable for Y velocity
+    public float jumpSpeed = 5;
+    // Start is called before the first frame update
+    public GameObject gameOverCanvas;
+    public int currentScore = 0;
+    public Text scoreText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("Pressed");
+            birdRB.velocity = new Vector2(birdRB.velocity.x, jumpSpeed);
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("Caw!");
+        gameOverCanvas.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //add one to the score
+        currentScore++;
+        scoreText.text = currentScore.ToString();
+    }
+}
---
+++ 3401_Week6_Brewer/Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public float spawnXPosition = 12;
+    public GameObject skyscrapperInstance;
+    public float spawnNewTriggerXPos = 0;
+
+    Transform lastSpawnedPipe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //instantiate skyscrapper at spawnpoint with set transform and rotation
+        //Instantiate(skyscrapperInstance, new Vector3(spawnXPosition, 0, 0), Quaternion.identity);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lastSpawnedPipe == null)
+        {
+            //instantiate skyscrapper at spawnpoint with set transform and rotation
+            lastSpawnedPipe = Instantiate (skyscrapperInstance, new Vector3(spawnXPosition, 0, 0), Quaternion.identity).transform;
+        }
+        else
+        {
+            if(lastSpawnedPipe.position.x < spawnNewTriggerXPos)
+            {
+                lastSpawnedPipe = Instantiate(skyscrapperInstance, new Vector3(spawnXPosition, 0, 0), Quaternion.identity).transform;
+            }
+        }
+    }
+}
